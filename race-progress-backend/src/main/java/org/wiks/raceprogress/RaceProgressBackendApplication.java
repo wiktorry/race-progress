f9d7d188dcdddacb368a46602b56c7b3ae1585e1
wiktorry <wiktorszulc4@gmail.com>
2025-05-25 20:12:18 +0200

    feat: init commit

---
+++ race-progress-backend/src/main/java/org/wiks/raceprogress/RaceProgressBackendApplication.java
@@ -0,0 +1,13 @@
+package org.wiks.raceprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceProgressBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceProgressBackendApplication.class, args);
+    }
+
+}
---
+++ race-progress-backend/src/test/java/org/wiks/raceprogress/RaceProgressBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wiks.raceprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceProgressBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
